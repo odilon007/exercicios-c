21ebeff15e454090cf94427c0613f33a3bedf772
Odilon Francelino de Pontes Neto <202514320015@ad.ifpb.edu.br>
2025-10-22 08:32:26 -0300

    primeira avaliacao

---
+++ c/avaliacoes/avaliacao1/questao4.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    // T = F * r
+    int ferramenta;
+    float forca, torque, raio;
+
+    printf("informe o tipo de ferramenta utilizada (1 = chave pequena, 2 = chave media, 3 = chave grande): ");
+    scanf("%d", &ferramenta);
+    printf("Digite o valor da forca aplicada: ");
+    scanf("%f", &forca);
+
+    switch (ferramenta) {
+        case 1:
+            raio = 0.1;
+            break;
+        case 2:
+            raio = 0.25;
+            break;
+        case 3:
+            raio = 0.40;
+            break;
+        default:
+            raio = 0;
+            break;
+    }
+
+    torque = forca * raio;
+
+    if (torque < 10) {
+        printf("Torque baixo.\n");
+    } else if (torque <= 25) {
+        printf("Torque adequado.\n");
+    } else {
+        printf("Torque excessivo – risco de dano!\n");
+    }
+
+    return 0;
+}
