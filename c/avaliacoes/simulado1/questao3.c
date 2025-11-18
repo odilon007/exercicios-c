b5ae6ea9a185a7fcc10d541c380e47710173ab9c
Odilon <odilonpontes7@gmail.com>
2025-11-18 08:29:33 -0300

    Adicionando simulado 1

---
+++ c/avaliacoes/simulado1/questao3.c
@@ -0,0 +1,35 @@
+
+#include <stdio.h>
+/*
+(2,5 pontos) Fazer um programa que leia um símbolo de operação do usuário (+, -, / ou *) e dois números
+reais. O programa deve retornar o resultado da operação recebida sobre estes dois números.*/
+int main()
+{
+    char op;
+    float num1, num2;
+    
+    printf("Digite o operador e os dois numeros: ");
+    scanf(" %c%f%f", &op, &num1, &num2);
+    
+    switch (op) {
+        case '+':
+            printf("%.1f + %.1f = %.1f", num1, num2, num1+num2);
+            break;
+        case '-':
+            printf("%.1f - %.1f = %.1f", num1, num2, num1-num2);
+            break;
+        case '*':
+            printf("%.1f * %.1f = %.1f", num1, num2, num1*num2);
+            break;
+        case '/':
+            if (num2 != 0) {
+                printf("%.1f / %.1f = %.2f", num1, num2, num1/num2);
+                break;
+            } else {
+                printf("Nao foi possivel realizar a divisao.\n");
+                break;
+            }
+    }
+    
+    return 0;
+}
